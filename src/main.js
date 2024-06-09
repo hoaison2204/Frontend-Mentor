2c4e71f8ae4ba90865731dd96b35e061e20f95b8
Nguyen Hoai Son <hoaison2204@gmail.com>
2024-06-09 10:10:22 +0700

    delete old source code

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-createApp(App).mount('#app')
