635ab61d7a283a8ff6d2fa8846a809a449c7840d
Nguyen Hoai Son <hoaison2204@gmail.com>
2024-06-09 22:17:09 +0700

    refactor base

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import QRCodeComponent from "./components/QR-code"
-function App() {
-	const heading: string = "Improve your front-end skills by building projects";
-	const description: string = "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level";
-	return (
-		<>
-			<QRCodeComponent 
-			heading={heading}
-			description={description}
-			/>
-		</>
-	)
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import router from './Router/Routes'
+
+const App: React.FC = () => {
+	return <RouterProvider router={router} />
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,31 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
+		prefix: 'tw-',
 		extend: {},
 		screens: {
 			desktop: '1440px',
 			mobile: '375px',
 		},
 		dropShadow: {
-			base: '0 25px 25px rgba(0, 0, 0, 0.0477)'
+			//QR-code
+			base: '0 25px 25px rgba(0, 0, 0, 0.0477)',
 		},
 		colors: {
 			white: 'hsl(0, 0%, 100%)',
 			lightGray: 'hsl(212, 45%, 89%)',
 			grayishBlue: 'hsl(220, 15%, 55%)',
 			darkBlue: 'hsl(218, 44%, 22%)',
+			roseWhite: 'hsl(330, 100%, 98%)',
+			eggshell: 'hsl(30, 54%, 90%)',
+			lightGrey: 'hsl(30, 18%, 87%)',
+			wengeBrown: 'hsl(30, 10%, 34%)',
+			darkCharcoal: 'hsl(24, 5%, 18%)',
+			nutmeg: 'hsl(14, 45%, 36%)',
+			darkRaspberry: 'hsl(332, 51%, 32%)',
 		},
 		fontSize: {
+			//QR-code
 			heading: ['24px', '32px'],
 			paragraph: ['17px', '24px'],
 		},
